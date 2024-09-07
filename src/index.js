6c7aff94d3da1a7f219106bff8d32c88275779b3
LE Quoc Dat <quocdat.le.insacvl@gmail.com>
2024-09-07 20:39:11 +0200

    fix: Create index.html and index.js files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MainInterface from './main-interface';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <MainInterface />
+  </React.StrictMode>
+);
