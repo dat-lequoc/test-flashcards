de0ff1ec6ddc600185cbed57c76d23ff2c36079d
LE Quoc Dat <quocdat.le.insacvl@gmail.com>
2024-09-07 21:17:15 +0200

    feat: Add MainInterface as the main page

---
+++ src/MainInterface.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MainInterface() {
+  return (
+    <div className="main-interface">
+      <h1>Welcome to the Main Interface</h1>
+      <p>This is the main page of our application.</p>
+    </div>
+  );
+}
+
+export default MainInterface;
